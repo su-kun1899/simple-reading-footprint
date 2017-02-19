3fb57f16eb089bb75d1ca4e08f943c224d0418de
su-kun1899 <higedrum.coz@gmail.com>
2017-02-20 00:39:37 +0900

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   get 'book_cards/index'
-
-  get 'book_cards/get'
+  get 'book_cards', to: 'book_cards#index'
 
   get 'book_cards/add'
+  post 'book_cards/add'
 
-  get 'book_cards/edit'
+  get 'book_cards/:id', to: 'book_cards#detail'
+
+  get 'book_cards/edit:id', to: 'cards#edit'
+  patch 'book_cards/edit/:id', to: 'book_cards#edit'
+
+  get 'book_cards/delete/:id', to: 'book_cards#delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
